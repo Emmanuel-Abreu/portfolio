397385697654186f1b3a91531c74f22a066a2078
emmanuel-abreu <28583353+Emmanuel-Abreu@users.noreply.github.com>
2023-12-21 22:45:22 -0500

    changed anchor element to Nextjs Link element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Attribution from './Attribution'
 import Icons from './Icons';
+import Link from 'next/link';
 //import '../assets/styles/Footer.css'
 
 export default function Footer(){
@@ -8,7 +9,7 @@ export default function Footer(){
     <footer className='Footer'>
       <Icons/>
       <Attribution/>
-      <p>Questions, comments, or feedback? Let me know! <a href='/Contact'>Send me an email</a></p>
+      <p>Questions, comments, or feedback? Let me know! <Link href='/Contact'>Send me an email</Link></p>
     </footer>
   );
 }
