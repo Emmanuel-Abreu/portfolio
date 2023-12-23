7f37db623d1d7d26466addb495e7e7553f67ea6f
emmanuel-abreu <28583353+Emmanuel-Abreu@users.noreply.github.com>
2023-12-22 22:15:02 -0500

    polished css

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Blog1 from '../../src/components/Blog1'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import '../../public/styles/Blog.css'
 
 export default function Blog(){
   return (
-    <div>
+    <div className='Blog'>
       <Navbar/>
       <Blog1/>
       <Footer/>
